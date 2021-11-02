b12f3c0bf5fcbc25bb28ac71c088b7737df4912d
Luis Escobar <lae311@stern.nyu.edu>
2021-11-02 16:21:13 -0400

    commit tree bde364ca86582b941b003a4740ad68dc5298e7b3

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+
+
+function ItemForm({onItemFormSubmit}) {
+  const [itemName, setItemName] = useState('')
+  
+  function addName (event) {
+    setItemName(event.target.value)
+  }
+  
+  const [itemCategory, setItemCategory] = useState('Produce')
+  
+  function addCategory (event) {
+    setItemCategory(event.target.value)
+  }
+
+function createItem (event) {
+  event.preventDefault();
+  onItemFormSubmit({
+    id: uuid(), // the `uuid` library can be used to generate a unique id
+    name: itemName,
+    category: itemCategory,
+  })
+}
+  
   return (
     <form className="NewItem">
       <label>
         Name:
-        <input type="text" name="name" />
+        <input value={itemName} onChange={addName}type="text" name="name" />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select value={itemCategory} onChange={addCategory} name="category">
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </label>
 
-      <button type="submit">Add to List</button>
+      <button onSubmit={createItem}type="submit">Add to List</button>
     </form>
   );
 }
